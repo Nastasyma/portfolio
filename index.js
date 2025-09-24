90e774c07c98d405ed7476e89dce994d46ac86cd
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2025-09-24 15:51:18 +0300

    feat: add accordion in faq section, add scrollbutton functional

--- index.js
+++ index.js
@@ -1,11 +1,70 @@
-import toggleBurgerMenu from './assets/js/burger.js';
+const body = document.querySelector('body');
+
+// burger
 
 const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__nav');
 const navItem = document.querySelectorAll('.header__nav-item');
-console.log(navItem);
+
+function toggleBurgerMenu() {
+  function addBurgerMenu() {
+    menu.classList.add('open');
+    burger.classList.add('active');
+    body.style.overflow = 'hidden';
+  }
+
+  function removeBurgerMenu() {
+    menu.classList.remove('open');
+    burger.classList.remove('active');
+    body.style.overflow = 'auto';
+  }
+
+  if (menu.classList.contains('open')) {
+    removeBurgerMenu();
+  } else {
+    addBurgerMenu();
+  }
+}
 
 burger.addEventListener('click', toggleBurgerMenu);
 
-navItem.forEach(item => {
+navItem.forEach((item) => {
   item.addEventListener('click', toggleBurgerMenu);
 });
+
+// scroll button
+
+const scrollBtn = document.querySelector('.scroll-button');
+const aboutSection = document.querySelector('#about');
+
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: aboutSection.offsetTop,
+    behavior: 'smooth',
+  });
+});
+
+// accordion
+
+const questionItem = document.querySelectorAll('.faq__content-questions-item');
+const question = document.querySelectorAll('.faq__content-questions-item-question');
+
+question.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    questionItem.forEach((item) => {
+      item.classList.remove('active');
+    });
+
+    item.parentElement.classList.toggle('active');
+
+    localStorage.setItem('_activeQuestionIndex', index);
+  });
+});
+
+const activeQuestionIndex = localStorage.getItem('_activeQuestionIndex');
+
+if (activeQuestionIndex) {
+  questionItem[activeQuestionIndex].classList.add('active');
+} else {
+  questionItem[0].classList.add('active');
+}
